490a079545c790e98fc5e69b9918acc35bdb2670
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-08 18:03:56 +0100

    Add player movement

---
+++ Scripts/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody
+
+var velocity = Vector3()
+var jump_strength = 5
+
+func _physics_process(delta):
+	velocity += Vector3.DOWN * 10 * delta
+	velocity.x *= 0.9
+	
+	velocity.x += int(Input.is_action_pressed("player_move_right")) - int(Input.is_action_pressed("player_move_left"))
+	
+	velocity += int(Input.is_action_pressed("player_move_jump") && is_on_floor()) * Vector3.UP * jump_strength
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
