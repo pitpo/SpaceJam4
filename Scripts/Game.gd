1e38b35d7e736df8bcad715c54bfca259bcab5d0
Tomasz Chabora <kobewi4e@gmail.com>
2018-12-08 18:40:47 +0100

    Fajne sterowanie :(

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -8,8 +8,6 @@ var layer = 0
 func _process(delta):
 	camera.translation.x += (player.translation.x - camera.translation.x)/10
 	
-	print(player.translation.z)
-#	print(-player.translation.z/4 * PI)
 	camera.get_child(0).translation.z = lerp(10, -10, -player.translation.z/4)
-	camera.get_child(0).translation.x = sin(-player.translation.z/4 * PI) * 10
+	camera.get_child(0).translation.x = -sin(-player.translation.z/4 * PI) * 10
 	camera.get_child(0).look_at(player.translation, Vector3.UP)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,15 +3,72 @@ extends KinematicBody
 var velocity = Vector3()
 var jump_strength = 5
 
+var x_action = 0
+var z_action = 0
+
 func _physics_process(delta):
 	velocity += Vector3.DOWN * 10 * delta
 	velocity.x *= 0.9
 	velocity.z *= 0.9
 	
-	velocity.x += int(Input.is_action_pressed("player_move_right")) - int(Input.is_action_pressed("player_move_left"))
-	velocity.z += int(Input.is_action_pressed("player_move_backward")) - int(Input.is_action_pressed("player_move_forward"))
+	var move_x = int(Input.is_action_pressed(action_right())) - int(Input.is_action_pressed(action_left()))
+	velocity.x += move_x
+	var move_z = int(Input.is_action_pressed(action_backward())) - int(Input.is_action_pressed(action_forward()))
+	velocity.z += move_z
 	
 	velocity += int(Input.is_action_pressed("player_move_jump") && is_on_floor()) * Vector3.UP * jump_strength
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	translation.z = clamp(translation.z, -4, 0)
+	
+	if translation.z < -1.3 and translation.z > -2.6:
+		if move_z == 0:
+			z_action = 1
+		if move_x == 0:
+			x_action = 1
+	elif translation.z < -2.6:
+		if move_z == 0:
+			z_action = 2
+		if move_x == 0:
+			x_action = 2
+	else:
+		if move_z == 0:
+			z_action = 0
+		if move_x == 0:
+			x_action = 0
+
+func action_forward():
+	match z_action:
+		0:
+			return "player_move_forward"
+		1:
+			return "player_move_left"
+		2:
+			return "player_move_backward"
+
+func action_backward():
+	match z_action:
+		0:
+			return "player_move_backward"
+		1:
+			return "player_move_right"
+		2:
+			return "player_move_forward"
+
+func action_right():
+	match x_action:
+		0:
+			return "player_move_right"
+		1:
+			return "player_move_forward"
+		2:
+			return "player_move_left"
+
+func action_left():
+	match x_action:
+		0:
+			return "player_move_left"
+		1:
+			return "player_move_backward"
+		2:
+			return "player_move_right"
