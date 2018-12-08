51b42a62d6b822ad79d628caca32791805f786a5
Tomasz Chabora <kobewi4e@gmail.com>
2018-12-08 19:27:23 +0100

    Zaorane fajne sterowanie ;_;

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,13 +1,9 @@
 extends Spatial
 
-onready var camera = $CameraRail
+onready var camera = $Camera
 onready var player = $Player
 
 var layer = 0
 
 func _process(delta):
 	camera.translation.x += (player.translation.x - camera.translation.x)/10
-	
-	camera.get_child(0).translation.z = lerp(10, -10, -player.translation.z/4)
-	camera.get_child(0).translation.x = -sin(-player.translation.z/4 * PI) * 10
-	camera.get_child(0).look_at(player.translation, Vector3.UP)
