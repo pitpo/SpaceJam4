aa0d29eafa0cf72ea825c492586b08d671717605
Tomasz Chabora <kobewi4e@gmail.com>
2018-12-08 20:38:30 +0100

    Czyszczonko

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -6,6 +6,9 @@ onready var player = $Player
 
 var layer = 0
 
+func _ready():
+	camera.translation.x = player.translation.x
+
 func _process(delta):
 	camera.translation.x += (player.translation.x - camera.translation.x)/10
 	paralax.get_child(1).motion_offset.x = -camera.translation.x * 10
