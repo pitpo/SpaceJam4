93d5651d6bc46eedfcac5b3e82b0d0ef29196dfb
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-09 02:52:06 +0100

    Main menu and new Alien anims

--- Scripts/Alien.gd
+++ Scripts/Alien.gd
@@ -5,9 +5,13 @@ var speed = 3
 var velocity = Vector3()
 var damage_cooldown = 0
 
+func _ready():
+	$Alien/AnimationPlayer.playback_speed = 3
+	$Alien/AnimationPlayer.get_animation("Walk").loop = true
+
 func _physics_process(delta):
 	if is_visible:
-		if !$Alien/AnimationPlayer.is_playing():
+		if $Alien/AnimationPlayer.current_animation != "ArmatureAction":
 			if abs(System.player.translation.x - translation.x) > 1 || abs(System.player.translation.y - translation.y) > 0.5:
 				var player_direction = sign(System.player.translation.x - translation.x)
 				velocity.x = player_direction
@@ -22,14 +26,14 @@ func _physics_process(delta):
 			else:
 				$Alien/AnimationPlayer.play("ArmatureAction")
 				$CollisionShape/AnimationPlayer.play("attac")
-				damage_cooldown = 1
+				damage_cooldown = 0.6
 		else:
 			rotation.y = -(Vector2(System.player.translation.x, System.player.translation.z) - Vector2(translation.x, translation.z)).rotated(-PI/2).angle()
 			damage_cooldown -= delta
 			var hit = move_and_collide(Vector3())
 			if damage_cooldown < 0 && hit != null && hit.collider == System.player:
 				System.game.ui.health -= 1
-				damage_cooldown = 1
+				damage_cooldown = 0.6
 	else:
 		velocity = Vector3()
 
@@ -46,4 +50,6 @@ func slowdown_disabled():
 	speed = 1.5
 
 func on_animation_finished(anim_name):
-	rotation_degrees.y = sign(System.player.translation.z - translation.z) * 90
+	rotation_degrees.y = sign(System.player.translation.x - translation.x) * 90
+	if anim_name == "ArmatureAction":
+		$Alien/AnimationPlayer.play("Walk")
