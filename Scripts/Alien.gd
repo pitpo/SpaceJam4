81d15414e7b1c41860b1ec8fd390b14d031f6894
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-09 01:58:59 +0100

    Make Llama alien attac

--- Scripts/Alien.gd
+++ Scripts/Alien.gd
@@ -6,14 +6,24 @@ var velocity = Vector3()
 
 func _physics_process(delta):
 	if is_visible:
-		var player_direction = sign(System.player.translation.x - translation.x)
-		velocity.x = player_direction
-		velocity.z = sign(System.player.translation.z - translation.z)
-		if player_direction < 0 && rotation_degrees.y > -90:
-			rotation_degrees.y -= delta * 300 * speed
-		elif player_direction > 0 && rotation_degrees.y < 90:
-			rotation_degrees.y += delta * 300 * speed
-		move_and_slide(velocity.normalized() * speed, Vector3.UP)
+		if !$Alien/AnimationPlayer.is_playing():
+			if abs(System.player.translation.x - translation.x) > 1:
+				var player_direction = sign(System.player.translation.x - translation.x)
+				velocity.x = player_direction
+				velocity.z = sign(System.player.translation.z - translation.z)
+				if player_direction < 0 && rotation_degrees.y > -90:
+					rotation_degrees.y -= delta * 300 * speed
+				elif player_direction > 0 && rotation_degrees.y < 90:
+					rotation_degrees.y += delta * 300 * speed
+				move_and_slide(velocity.normalized() * speed, Vector3.UP)
+			else:
+				$Alien/AnimationPlayer.play("ArmatureAction")
+				$CollisionShape/AnimationPlayer.play("attac")
+		else:
+			rotation.y = -(Vector2(System.player.translation.x, System.player.translation.z) - Vector2(translation.x, translation.z)).rotated(-PI/2).angle()
+			var hit = move_and_collide(Vector3())
+			if hit != null && hit.collider == System.player:
+				System.game.ui.hearts -= 1
 	else:
 		velocity = Vector3()
 
